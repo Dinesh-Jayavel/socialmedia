6774cfd1550d566f8292548dc0cea4208bc42437
dinesh jayavel <dineshjayavel030901@gmail.com>
2023-08-11 08:11:42 +0530

    Project Completed

---
+++ ui/src/components/topbar/Topbar.jsx
@@ -0,0 +1,69 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import{Link}from"react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+
+export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+      <Link to="/" style={{textDecoration:"none"}}>
+      <span className="logo">SocialCircle</span>
+      </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">
+            <Link to="/" style={{textDecoration:"none",color:"white"}}>Home</Link>
+          </span>
+          <span className="topbarLink">
+          <Link to={`/timeline/${user._id}`} 
+          style={{textDecoration:"none",color:"white"}}>Timeline</Link>
+           </span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+          <Link to={`/profile/${user.username}`} 
+          style={{textDecoration:"none",color:"white"}}>
+            <Person />
+            {/* <span className="topbarIconBadge">1</span> */}
+            </Link>
+          </div>
+          <div className="topbarIconItem">
+          <Link to="/messenger" style={{textDecoration:"none",color:"white"}}>
+            <Chat />
+            {/* <span className="topbarIconBadge">2</span> */}
+            </Link>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            {/* <span className="topbarIconBadge">1</span> */}
+          </div>
+        </div>
+        <Link to={`/profile/${user.username}`}>
+          <img src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+          </Link>
+      </div>
+    </div>
+  );
+}
